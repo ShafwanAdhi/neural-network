df50e6c15e739628f4c7f33c1454c107dc3b5316
Shafwan Adhi Dwi Nugraha <tugashafwan@gmail.com>
2025-11-29 17:20:53 +0700

    Create __init__.py

---
+++ neural_network/__init__.py
@@ -0,0 +1,21 @@
+```python
+"""
+Neural Network From Scratch
+A simple neural network implementation using only NumPy
+"""
+
+__version__ = "0.1.0"
+__author__ = "Your Name"
+
+from neural_network.core.network import NeuralNetwork
+from neural_network.activations.functions import ActivationFunctions
+from neural_network.losses.functions import LossFunctions
+from neural_network.core.initializers import WeightInitializers
+
+__all__ = [
+    "NeuralNetwork",
+    "ActivationFunctions",
+    "LossFunctions",
+    "WeightInitializers",
+]
+```

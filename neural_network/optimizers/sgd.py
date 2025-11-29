1d3bd57e460363671aed4f3b3254e910927cba9b
Shafwan Adhi Dwi Nugraha <tugashafwan@gmail.com>
2025-11-29 18:16:56 +0700

    Create sgd.py

---
+++ neural_network/optimizers/sgd.py
@@ -0,0 +1,33 @@
+```python
+"""
+Stochastic Gradient Descent Optimizer
+(Placeholder for future optimizer implementations)
+"""
+
+
+class SGD:
+    """
+    Stochastic Gradient Descent optimizer
+    Currently integrated into NeuralNetwork class
+    """
+    
+    def __init__(self, learning_rate=0.01):
+        self.learning_rate = learning_rate
+    
+    def update(self, params, gradients):
+        """
+        Update parameters using SGD
+        
+        Args:
+            params: Current parameters
+            gradients: Computed gradients
+            
+        Returns:
+            updated_params: Updated parameters
+        """
+        # Simple SGD update: param = param - lr * gradient
+        updated_params = []
+        for param, grad in zip(params, gradients):
+            updated_params.append(param - self.learning_rate * grad)
+        return updated_params
+```

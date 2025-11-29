ce9d9519f601987b23e80e926b41bb32c5f7948f
Shafwan Adhi Dwi Nugraha <tugashafwan@gmail.com>
2025-11-29 17:22:01 +0700

    Create __init__.py

---
+++ neural_network/core/__init__.py
@@ -0,0 +1,6 @@
+```python
+from neural_network.core.network import NeuralNetwork
+from neural_network.core.initializers import WeightInitializers
+
+__all__ = ["NeuralNetwork", "WeightInitializers"]
+```

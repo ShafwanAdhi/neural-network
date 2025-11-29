db01e6232190c5cb9ca18ba7f027d7c8ee955ca2
Shafwan Adhi Dwi Nugraha <tugashafwan@gmail.com>
2025-11-29 17:25:11 +0700

    Create functions.py

---
+++ neural_network/activations/functions.py
@@ -0,0 +1,51 @@
+```python
+"""
+Activation Functions and Their Derivatives
+"""
+
+import numpy as np
+
+
+class ActivationFunctions:
+    """Class for storing all activation functions and derivatives"""
+    
+    @staticmethod
+    def sigmoid(num):
+        """Sigmoid activation function"""
+        return 1 / (1 + np.exp(-1 * num))
+    
+    @staticmethod
+    def der_sigmoid(x):
+        """Derivative of sigmoid"""
+        return x * (1 - x)
+    
+    @staticmethod
+    def tanh(num):
+        """Tanh activation function"""
+        return (np.exp(num) - np.exp(-num)) / (np.exp(num) + np.exp(-num))
+    
+    @staticmethod
+    def der_tanh(x):
+        """Derivative of tanh"""
+        return 1 - x ** 2
+    
+    @staticmethod
+    def relu(num):
+        """ReLU activation function"""
+        return max(0, num)
+    
+    @staticmethod
+    def der_relu(x):
+        """Derivative of ReLU"""
+        return 1 if x > 0 else 0
+    
+    @staticmethod
+    def leaky_relu(num):
+        """Leaky ReLU activation function"""
+        return max(0.1 * num, num)
+    
+    @staticmethod
+    def der_leaky_relu(x):
+        """Derivative of Leaky ReLU"""
+        return 1 if x > 0 else 0.1
+```
